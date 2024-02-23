edb7ea85e576792caed2c4e6b83f976b032545ef
Teoman ATEŞ <80816340+teomanates@users.noreply.github.com>
2024-02-23 23:02:46 +0300

    port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -6,14 +6,13 @@ import socket
 from datetime import datetime
 
 #Define our target
-"""print(dir(sys)) yaparak neleri içerdiğini görebilriz."""
 if len(sys.argv) == 2:
 	target = socket.gethostbyname(sys.argv[1]) #translate a host name to IPV4
 else:
 	print("Invalid amount of arguments")
 	print("Syntax: python3 scanner.py <ip>")
 	sys.exit()
-#Add a pretty banner		
+#Add a banner		
 print("-"*50)
 print("Scanning target {} ".format(target))
 print("Time started: "+str(datetime.now()))
@@ -21,7 +20,7 @@ print("-"*50)
 
 try:
 	for port in range(1,499 ):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4 and tcp conne+ction
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4 and tcp connection
 		socket.setdefaulttimeout(2) 
 		result = s.connect_ex((target,port)) #returns error indicator
 		print("Checking port {}".format(port))
